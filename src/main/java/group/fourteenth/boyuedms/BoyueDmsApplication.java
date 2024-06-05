2c62c5477bdbee71f4b7d645a6fee1acc43f584d
Moetaki <skyvoy@moetaki.com>
2024-06-05 18:33:42 +0800

    Initial commit

---
+++ src/main/java/group/fourteenth/boyuedms/BoyueDmsApplication.java
@@ -0,0 +1,13 @@
+package group.fourteenth.boyuedms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyueDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoyueDmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/fourteenth/boyuedms/BoyueDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package group.fourteenth.boyuedms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyueDmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
